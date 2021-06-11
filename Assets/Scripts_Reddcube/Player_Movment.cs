50f9ca67abf23e282149915a913b69d84674762e
REDDCUBE1 <marwan.ashraf1333@gmail.com>
2021-06-12 00:11:53 +0200

    lol hh

---
+++ Assets/Scripts_Reddcube/Player_Movment.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movment : MonoBehaviour
+{
+
+    public float speed = 10f;
+
+    Vector2 movment;
+
+    public Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        movment.x = Input.GetAxisRaw("Horizontal");
+
+        movment.y = Input.GetAxisRaw("Vertical");
+    }
+
+
+    private void FixedUpdate()
+    {
+
+        rb.MovePosition(rb.position + movment * speed * Time.fixedDeltaTime);
+
+    }
+}
