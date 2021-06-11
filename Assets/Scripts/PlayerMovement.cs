2cda7d6aec628226222a7cfcae6a56a840dd5a03
nikinov <novellenicholas@gmail.com>
2021-06-12 00:23:16 +0200

    version fix

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,19 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_Movment : MonoBehaviour
+public class PlayerMovment : MonoBehaviour
 {
 
     public float speed = 10f;
 
-    Vector2 movment;
-
-    public Rigidbody2D rb;
+    [SerializeField] private Rigidbody2D rb;
+    
+    private Vector2 movment;
 
     // Update is called once per frame
     void Update()
     {
-
         movment.x = Input.GetAxisRaw("Horizontal");
 
         movment.y = Input.GetAxisRaw("Vertical");
@@ -23,8 +22,6 @@ public class Player_Movment : MonoBehaviour
 
     private void FixedUpdate()
     {
-
-        rb.MovePosition(rb.position + movment * speed * Time.fixedDeltaTime);
-
+        rb.MovePosition(rb.position + movment * (speed * Time.fixedDeltaTime));
     }
 }
