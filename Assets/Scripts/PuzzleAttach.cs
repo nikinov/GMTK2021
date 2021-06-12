7a55ae77fc5ca8ffb7d63354aeee5370f7c0edb5
REDDCUBE1 <marwan.ashraf1333@gmail.com>
2021-06-12 02:08:28 +0200

    Reddcube update
    i got the ground mechanics down the movement and the puzzle drag and the replay or exit it looks like literal shit tbh but its working i didnt use the event but the death is a function you can take the event part i didnt wanna mess it up and thats all for this and like sorry XD.

---
+++ Assets/Scripts/PuzzleAttach.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleAttach : MonoBehaviour
+{
+
+    Vector2 pos;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get mouse input in pixels
+        Vector3 mouse = Input.mousePosition;
+       
+        
+        //turning mouse pixel position to world space vectors
+        pos = Camera.main.ScreenToWorldPoint(mouse);
+
+        
+       
+    }
+    
+      
+    //setting the puzzle to mouse position on drag it works like a charm away from how stupid btw XD
+    private void OnMouseDrag()
+    {
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/Replaying.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Replaying : MonoBehaviour
+{
+
+    /// <summary>
+    /// replaying and exit functions for the buttons
+    /// </summary>
+    public void replay()
+    {
+
+        SceneManager.LoadScene("sc1");
+
+    }
+
+    public void Exit()
+    {
+
+        Application.Quit();
+
+    }
+
+}
