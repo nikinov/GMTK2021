5eed57990e7fccd3556c81756ef0284da1ed8d87
REDDCUBE1 <marwan.ashraf1333@gmail.com>
2021-06-13 19:26:10 +0200

    main menu

--- Assets/Scripts/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -1,18 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MainMenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public GameObject plane;
+
+
+
+    private void Start()
     {
-        
+        plane.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    public void play()
+  {
+
+        SceneManager.LoadScene("sc2");
+
+  }
+
+
+
+    public void quit()
     {
-        
+
+
+        Application.Quit();
+
+
+
+    }
+
+
+
+    public void credits()
+    {
+
+
+        SceneManager.LoadScene("credits");
+
+
+    }
+
+    public void ok()
+    {
+
+        plane.SetActive(false);
+
     }
 }
